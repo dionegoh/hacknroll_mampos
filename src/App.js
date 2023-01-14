6ca03304c5d2b273b48b6d286f6dfe2e2880dbd7
dionegoh <e0439283@u.nus.edu>
2023-01-14 17:13:40 +0800

    add datetimestring

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Clock from './components/clock';
+import DateTimeString from './components/datetimestring';
 import { Grid, GridItem } from '@chakra-ui/react'
 
 function App() {
   return (
     <Grid h="100vh" 
-          templateColumns='repeat(2, 1fr)'
-          templateRows='repeat(5, 1fr)'
+          templateColumns='repeat(3, 1fr)'
+          templateRows='repeat(10, 1fr)'
           gap={4}>
-      <GridItem colSpan={1} rowSpan={4} >
+      <GridItem colSpan={1} rowSpan={6} >
         <Clock></Clock>
       </GridItem>
-      <GridItem colSpan={1}>
+      <GridItem colSpan={2} rowSpan={10} bg='tomato' >
+      </GridItem>
+
+      <GridItem colSpan={1} rowSpan={4} textAlign={'center'}>
+        <DateTimeString></DateTimeString>
      </GridItem>
     </Grid>
             
