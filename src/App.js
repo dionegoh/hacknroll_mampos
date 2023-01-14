57eefe00335edf5ab46f73f62a9a42298fbc2e0a
dionegoh <e0439283@u.nus.edu>
2023-01-14 16:12:04 +0800

    add chakra grid and align clock

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Clock from './components/clock';
+import { Grid, GridItem } from '@chakra-ui/react'
 
 function App() {
   return (
-        
-    <Clock></Clock>
+    <Grid h="100vh" 
+          templateColumns='repeat(2, 1fr)'
+          templateRows='repeat(5, 1fr)'
+          gap={4}>
+      <GridItem colSpan={1} rowSpan={4} >
+        <Clock></Clock>
+      </GridItem>
+      <GridItem colSpan={1}>
+     </GridItem>
+    </Grid>
+            
 
   );
 }
